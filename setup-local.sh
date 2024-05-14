fc906a5f86b1cce4ff42f4bb52084817c4dc4306
Ray <bdhmiloo@users.noreply.github.com>
2024-05-14 13:36:56 +0800

    feat: update setup-local.sh

--- setup-local.sh
+++ setup-local.sh
@@ -13,15 +13,32 @@ success "Done"
 
 step "Installing development tools..."
 brew install --cask iterm2
-# brew install docker
-# brew install git
 brew install --cask raycast
 brew install --cask quicklook-json
+brew install --cask aws-vault
+brew install --cask visual-studio-code
+brew install --cask intellij-idea
+brew install --cask intellij-idea-ce
+brew install docker
+brew install git
+success "Done"
+
+step "Installing util tools..."
+brew install --cask obs
+brew install --cask licecap
+brew install --cask rectangle
 success "Done"
 
 step "Installing communication tools..."
 brew install --cask discord
 brew install --cask zoom
+brew install --cask linear-linear
+success "Done"
+
+step "Installing security tools..."
+brew install --cask keepassxc
+brew install --cask tailscale
+brew install --cask cryptomator
 success "Done"
 
 echo "Installing nvm..."
@@ -31,3 +48,14 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 success "Done"
 
+echo "Optimizing mac settings..."
+
+# snappy dock
+defaults write com.apple.dock autohide-delay -int 0
+defaults write com.apple.dock autohide-time-modifier -float 0.4
+killall Dock
+
+# touch id for sudo
+sed "s/^#auth/auth/" /etc/pam.d/sudo_local.template | sudo tee /etc/pam.d/sudo_local 
+
+success "Done"
