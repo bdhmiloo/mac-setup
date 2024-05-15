c8125e77ac5c3d9f9339c896370a4bbe67e13690
Ray <bdhmiloo@users.noreply.github.com>
2024-05-15 15:25:54 +0800

    feat: add aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# general
+alias lhidden="ls -d .* --color=auto" # show hidden files
+alias c="clear"
+
+# config
+alias zshcfg="nano ~/.zshrc"
+alias ohmyzsh="nano ~/.oh-my-zsh"
+alias sshcfg="nano ~/.ssh/config"
+alias zshshortcut="nano ~/.oh-my-zsh/custom/shortcuts.zsh"
+alias zshalias="nano ~/.oh-my-zsh/custom/aliasis.zsh"
+
+# history
+alias h="history"
+alias gh="history | grep"
+
+# date
+alias now="date +"%T""
+alias nowtime=now
+alias nowdate="date +"%d-%m-%Y""
+
+# confirmation
+alias ln="ln -i"
+alias rm="rm -I –preserve-root"
